c47e660dc6384d7c912e6a7f8c218da3f9e36620
bel4 <yazdannaderi@hotmail.com>
2025-12-13 18:53:30 +0330

    Ah

--- scripts/setup-vps.sh
+++ scripts/setup-vps.sh
@@ -108,6 +108,19 @@ server {
     listen 80;
     listen [::]:80;
     server_name ${FRONTEND_DOMAIN} www.${FRONTEND_DOMAIN};
+    return 301 https://\$server_name\$request_uri;
+}
+
+server {
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+    server_name ${FRONTEND_DOMAIN} www.${FRONTEND_DOMAIN};
+
+    # SSL certificate (will be configured by Certbot)
+    ssl_certificate /etc/letsencrypt/live/${API_DOMAIN}/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/${API_DOMAIN}/privkey.pem;
+    include /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
 
     location / {
         proxy_pass http://127.0.0.1:3000;
