dc3a48fc60d5dd74ed059edd979dbfb8b965c44d
bel4 <yazdannaderi@hotmail.com>
2025-12-13 18:39:37 +0330

    Kms

--- backend/matcher_backend/settings.py
+++ backend/matcher_backend/settings.py
@@ -178,6 +178,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (user uploads)
 MEDIA_ROOT = BASE_DIR / 'media'
